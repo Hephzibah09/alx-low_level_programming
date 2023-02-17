81da34515a2efb966ffb9048dbd084c08c23127d
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-16 16:23:51 -0800

    programming is like a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using the puts function
+ *
+ * Retun - 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
