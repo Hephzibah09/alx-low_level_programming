cd9343e9497cb94eaeaec05ae3fde28b09743c15
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-21 11:58:18 -0800

    it is the absolue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: the integer variable
+ *
+ * Return: program returns 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+		return (n);
+}
