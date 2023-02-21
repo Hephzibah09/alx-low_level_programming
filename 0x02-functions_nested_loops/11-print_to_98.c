06b49c708f989246df8359f85be342fb3fb11c28
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-21 13:37:16 -0800

    up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,23 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
-		printf("%d", n);
-	while (n != 98)
+	while (n <= 98 || n >= 98)
 	{
-		printf("%d", n);
-		if (n != 98)
+		if (n == 98)
+			printf("%d", n);
+		else if (n < 98 || n > 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (n > 98)
+				n--;
+			else
+				n++;
 		}
-		if (n > 98)
-			n--;
-		else
-			n++;
 	}
 	_putchar('\n');
 }
