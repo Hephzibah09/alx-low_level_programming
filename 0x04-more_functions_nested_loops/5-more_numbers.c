1372049bf1d556a97abdae5f2b4c9ab806daafd6
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-23 15:23:10 -0800

    more and most numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints out numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int n, num;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (num = 0; num <= 14; n++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
