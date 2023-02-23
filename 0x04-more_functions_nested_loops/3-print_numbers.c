2a7c4e1903549f4e0243c04602c695f4098298cb
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-23 15:07:27 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,16 @@
 
 /**
  * print_numbers - program prints numbers from 0 to 9
+ * program takes no arguments
+ *
+ * Return: program returns nothing
  */
 
 void print_numbers(void)
 {
-	for (int a = '0'; a <= '9'; a++)
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
 	{
 		_putchar(a);
 	}
