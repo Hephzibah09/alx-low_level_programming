7adca308344cf0985dd87f3f50d125eea405247b
Hephzibah09 <samuel22adetula@gmail.com>
2023-02-23 14:56:38 -0800

    products and numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - gets the product of two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: program returns the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - program prints numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	for (int a = '0'; a <= '9'; a++)
+	{
+		_pitchar(a);
+	}
+	_putchar('\n');
+}
