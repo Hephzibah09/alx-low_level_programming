8ffc33a2850dbdbc1b446e91bdf1b0ab0e756217
Hephzibah09 <samuel22adetula@gmail.com>
2023-03-02 13:16:08 -0800

    0ne to four

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,15 +12,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	;
-
-	for (j = 0; j <= n; j++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[i + j] = src[j];
+		i++;
 	}
-
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
