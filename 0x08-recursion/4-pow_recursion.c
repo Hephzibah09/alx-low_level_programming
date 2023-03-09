a2ce05099070f0c0165bfeff63f0468ed16d839f
Hephzibah09 <samuel22adetula@gmail.com>
2023-03-09 13:32:38 -0800

    exponential recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - program calculates the value of x to the power of y
+ * @x: integer value/variable
+ * @y: integer value/variable
+ *
+ * Return: always return integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (x == 0)
+		return (0);
+	if (!(y < 0))
+		return (x * _pow_recursion(x, y - 1));
+}
