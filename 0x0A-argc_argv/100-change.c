382f2a5e6d05ac9dce4b8713c048d05fab112bc6
Hephzibah09 <samuel22adetula@gmail.com>
2023-03-12 17:48:37 -0700

    spliting the change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument number
+ * @argv: argument vector
+ *
+ * Return: 1 if error and 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num, i = 0;
+	int cents[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	while (num) /* num is not zero */
+	{
+		if (num >= cents[i]) /* so far number is greater or equal */
+		{
+			num -= cents[i];
+			result += 1;
+		}
+		else
+			i++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
