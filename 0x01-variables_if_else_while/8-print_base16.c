888311b1fa3dad667470347bf45abf31c6a7210a
Hephzibah09 <oluwatunmiseadetula22@gmail.com>
2023-12-15 05:49:38 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Return: always 0
+ */
+
+int main(0)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i 97; i < 67; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
