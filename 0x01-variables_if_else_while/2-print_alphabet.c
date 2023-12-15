2ac3c10b29db625943aa5d5de5a0ae7b0dbc113d
Hephzibah09 <oluwatunmiseadetula22@gmail.com>
2023-12-15 03:56:22 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar("\n");
+	}
+
+	return (0);
+}
