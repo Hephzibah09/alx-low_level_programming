89626405c382b6b09af6dc10ea96e76dc86eb1b5
Hephzibah09 <oluwatunmiseadetula22@gmail.com>
2023-12-15 03:15:02 -0800

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/* the conditional statement to check if the number is positive, negative or zero */
+	/* the conditional statement to
+	 * check if the number is positive, negative or zero
+	 */
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n > 0)
