74f8433816f40e7d18f47ce9be9f445b249c64b6
Hephzibah09 <oluwatunmiseadetula22@gmail.com>
2024-01-21 01:49:17 -0800

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * putchar - this function prints the string "_putchar", with a new line
+ *
+ * Return: the program should return 0
+ */
+
+int putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int putchar(void);
+int _putchar(char c);
+
+#endif
