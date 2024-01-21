622d5772d2826378861644ae8abd8f10711fbc17
Hephzibah09 <122885570+Hephzibah09@users.noreply.github.com>
2024-01-21 17:15:35 +0100

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,12 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || (year % 40 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2)
+		{
 			day++;
+		}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
@@ -22,8 +24,12 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		if (month == 2 && day == 60)
+		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
 		else
+		{
 			printf("Day of the year: %d\n", 365 - day);
+		}
 	}
 }
