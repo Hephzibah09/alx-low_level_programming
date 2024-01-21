aff73137dad216ff57ab00c508edce4572ce6e50
Hephzibah09 <122885570+Hephzibah09@users.noreply.github.com>
2024-01-21 14:09:31 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,8 @@ void print_to_98(int n)
 {
 	
     if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
+	int i;
+        for (i = n; i <= 98; i++) {
             printf("%d", i);
 
             if (i < 98) {
@@ -18,7 +19,7 @@ void print_to_98(int n)
             }
         }
     } else {
-        for (int i = n; i >= 98; i--) {
+        for (i = n; i >= 98; i--) {
             printf("%d", i);
 
             if (i > 98) {
